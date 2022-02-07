d11da0aa8ddf7669f5045ba1f8f502db15d5a9b6
dimitrirutz <dimitri@drutz.ch>
2022-02-07 10:47:11 +0100

    Masquage au clique du modal du panier

--- assets/modules/cart.js
+++ assets/modules/cart.js
@@ -1,7 +1,28 @@
+import {isTarget} from "./polyfill";
+
 export class Cart {
 
+    /**
+     * @param {HTMLLIElement} cartHeader
+     */
+    constructor(cartHeader) {
+        this.cartHeader = cartHeader
+        this.button = this.cartHeader.querySelector(".js-cart-toggle")
+        this.cart = this.cartHeader.querySelector(".js-cart-container")
+        this.button.addEventListener("click", this.handleClick)
 
-    
+        document.addEventListener("click", (e) => {
+            if(!isTarget(e, this.cart) && this.cart.classList.contains("visible")){
+                this.cart.classList.remove("visible")
+            }
+        })
+    }
+
+    handleClick = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        this.cart.classList.toggle("visible")
+    }
 
 
 }
--- assets/modules/polyfill.js
+++ assets/modules/polyfill.js
@@ -1,3 +1,8 @@
 export function strToDom(str) {
     return document.createRange().createContextualFragment(str)
 }
+
+
+export const isTarget = (e, target) => {
+    return e.path.includes(target)
+}
