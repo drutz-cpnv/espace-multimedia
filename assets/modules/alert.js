178cb05162bfffe33425366d16c73a76f9030e18
dimitrirutz <dimitri@drutz.ch>
2022-04-07 12:04:28 +0200

    Refonte alerte statique

--- assets/modules/alert.js
+++ assets/modules/alert.js
@@ -1,20 +1,45 @@
+import {strToDom} from "../functions/dom";
 
 export default class Alert extends HTMLElement {
 
-
-
     constructor() {
         super();
+
+        const message = this.getAttribute('message')
+        const title = this.getAttribute('alert-title')
+
+        const header = strToDom(`<div class="alert-header">
+        <div class="alert__title">
+            <span class="icon material-icons-round">warning</span>
+            <span class="alert__title-title">${title}</span>
+        </div>
+        <div class="alert-minimize">
+            <a href="#" class="alert__minimize-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+            </a>
+        </div>
+    </div>`)
+
+        const body =  strToDom(`<div class="alert-body">
+        <p>${message}</p>
+    </div>`)
+
+
+        console.log(body)
+        console.log(header)
+        this.append(header)
+        this.append(body)
+
+
         this.header = this.querySelector(".alert-header")
         this.btn = this.querySelector(".alert__minimize-btn")
         this.body = this.querySelector(".alert-body")
 
         this.isOpen = !this.classList.contains("closed");
 
-        console.log(this.header.clientHeight)
-
         this.defaultHeight = this.header.clientHeight
         const bodyHeight = this.body.clientHeight
+
         this.openedHeight = this.defaultHeight + bodyHeight
 
         if(this.isOpen) {
@@ -30,6 +55,9 @@ export default class Alert extends HTMLElement {
             e.preventDefault()
             this.toggle()
         })
+    }
+
+    connectedCallback() {
     }
 
     toggle = () => {
